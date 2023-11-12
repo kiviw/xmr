2ad18e806276f9fa534c4e2e2c7e1e783682d006
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-12 14:41:01 +0300

    Update countdown-timer.js

--- countdown-timer.js
+++ countdown-timer.js
@@ -1,8 +1,27 @@
-jQuery(document).ready(function($) {
-    // Countdown Timer
-    var orderID = countdown_timer_data.order_id;
-    var expirationTime = countdown_timer_data.expiration_time;
+// File: countdown-timer.js
+jQuery(document).ready(function ($) {
+    // Set the date we're counting down to
+    var countDownDate = new Date(countdown_timer_data.expiration_time * 1000).getTime();
 
-    // Your logic to initialize and display the countdown timer
-    // You can use a library like Countdown.js or build your own logic
+    // Update the countdown every 1 second
+    var x = setInterval(function () {
+        // Get the current date and time
+        var now = new Date().getTime();
+
+        // Calculate the remaining time
+        var distance = countDownDate - now;
+
+        // Calculate minutes and seconds
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the countdown timer
+        document.getElementById("countdown-timer").innerHTML = minutes + "m " + seconds + "s ";
+
+        // If the countdown is over, redirect to the shop page
+        if (distance < 0) {
+            clearInterval(x);
+            window.location.href = '<?php echo wc_get_page_permalink("shop"); ?>';
+        }
+    }, 1000);
 });
