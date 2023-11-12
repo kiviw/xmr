96e4a94b9ca4dce1a0628d87a62d3a994e4232a5
kiviw <100179542+kiviw@users.noreply.github.com>
2023-11-12 14:24:46 +0300

    Create countdown-timer.js

---
+++ countdown-timer.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+    // Countdown Timer
+    var orderID = countdown_timer_data.order_id;
+    var expirationTime = countdown_timer_data.expiration_time;
+
+    // Your logic to initialize and display the countdown timer
+    // You can use a library like Countdown.js or build your own logic
+});
